1dbb4fe991726bb1f157ddf4cc9aa5f5a166c24e
Adam Levinson <adamlev513@gmail.com>
2025-05-14 13:49:39 +0300

    Update text in AboutCard component

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,10 +9,10 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Adam Lev </span>
-            from <span className="purple"> Israel.</span>
+            Hi everyone, I'm <span className="purple">Adam Lev</span> from{" "}
+            <span className="purple">Israel</span>.
             <br />
-            I am currently employed as a NOC operator at{" "}
+            I'm currently working as a NOC operator at{" "}
             <a
               href="https://service.we-com.co.il/he/information/about?_gl=1*1bjgv3v*_gcl_aw*R0NMLjE3NDAwNzQ4NDcuRUFJYUlRb2JDaE1Jclk2YzktdlNpd01WdDF0QkFoMEROUmxNRUFBWUFTQUFFZ0w0TVBEX0J3RQ..*_gcl_au*NDM3MjEyNzYzLjE3NDAwNzQ4NDc.*_ga*Nzg2OTcyMDUuMTc0MDA3NDg0Nw..*_ga_BFLMXPCMWK*MTc0MDA3NDg0Ny4xLjAuMTc0MDA3NDg0Ny42MC4wLjA."
               target="_blank"
@@ -23,11 +23,24 @@ function AboutCard() {
             </a>
             .
             <br />
-            I have AWS Certified DevOps Engineer - Professional and CKA certification.
-            I also started a degree in Computer Science at the Open University.
+            I'm currently preparing for the{" "}
+            <span className="purple">
+              AWS Certified DevOps Engineer – Professional
+            </span>{" "}
+            and{" "}
+            <span className="purple">
+              CKA (Certified Kubernetes Administrator)
+            </span>{" "}
+            certifications, while continuously building hands-on skills in
+            DevOps and cloud technologies.
+            <br />
+            I’ve also recently started a{" "}
+             Bachelor’s degree in
+            <span className="purple"> Computer Science</span> at the{" "}
+            Open University of Israel.
             <br />
             <br />
-            Apart from coding, here are some activities that I love to do!
+            Apart from coding, here are some activities that I love to do:
           </p>
           <ul>
             <li className="about-activity">
